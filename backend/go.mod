e4c07f5a90ff9774af2de8726c6e955026da6aec
JorgeAndresQuintero <jorge_a_q123@hotmail.com>
2020-12-01 16:42:07 +0100

    add redis container and small connection example

--- backend/go.mod
+++ backend/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/cossacklabs/themis/gothemis v0.13.1
 	github.com/gorilla/mux v1.8.0
+	github.com/go-redis/redis/v8 v8.4.0
 )
--- backend/main.go
+++ backend/main.go
@@ -10,14 +10,50 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	server "github.com/cyla-app/cyla-app/server"
+	redis "github.com/go-redis/redis/v8"
 	"log"
 	"net/http"
-
-	server "github.com/cyla-app/cyla-app/server"
+	"os"
 )
+
+var ctx = context.Background()
+func ExampleClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: os.Getenv("REDIS_PASSWORD"), // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
 
 func main() {
 	log.Printf("Server started")
+	ExampleClient()
+	log.Printf("Other stuff")
 
 	ActivityApiService := server.NewActivityApiService()
 	ActivityApiController := server.NewActivityApiController(ActivityApiService)
