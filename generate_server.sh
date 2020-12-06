e44c209eba7997c7fa620b0ea2e0779ee53e6be8
JorgeAndresQuintero <jorge_a_q123@hotmail.com>
2020-12-06 10:54:27 +0100

    change createUser endpoint to return the new UUID. template improvements

--- generate_server.sh
+++ generate_server.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+GO_SOURCE_PATH="backend"
+
+# Name for the package and location of the generated server stub
+GEN_GO_PACKAGE_NAME="server"
+
 # Uses the openapi generator (https://github.com/OpenAPITools/openapi-generator) to generate a stub server using docs/openapi-specification.yaml
-# Most output files are saved at backend/server, except the generated main.go.
+# Most output files are saved at GO_SOURCE_PATH/server.
 set -eo pipefail
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
 	-i /local/openapi/openapi-specification.yaml \
 	-g go-server \
 	-t /local/openapi/go-server_templates \
-	-o /local/backend \
+	-o /local/${GO_SOURCE_PATH} \
 	--git-repo-id cyla-app --git-user-id cyla-app \
-	--package-name server \
-	--additional-properties=serverPort=5000,sourceFolder=server
+	--package-name ${GEN_GO_PACKAGE_NAME} \
+	--additional-properties=serverPort=5000,sourceFolder=${GEN_GO_PACKAGE_NAME}
+
+# Remove the api folder where usually the api specification is saved.
+rmdir "${GO_SOURCE_PATH}/api"
 
 # openapi-generator add some redundant dependencies. We use goimports (https://godoc.org/golang.org/x/tools/cmd/goimports) to clean those imports.
-goimports -w backend/**/*.go
-# Remove the api folder where usually the api specification is saved.
-rmdir backend/api
-
+goimports -w ${GO_SOURCE_PATH}/**/*.go
 	 
