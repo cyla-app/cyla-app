b23016af43eb195b9bdfbd6f28e592d4eb364dc3
Porrum <erik.pallas@student.uni-augsburg.de>
2020-12-11 21:53:59 +0100

    worked on data entry component

--- frontend/src/components/DayDataEntry.tsx
+++ frontend/src/components/DayDataEntry.tsx
@@ -9,11 +9,66 @@ import { Day } from '../../generated'
 
 type ExcludeReasonValue = 'not defined' | 'sick' | 'sleep' | 'hungover'
 type BleedingValue = 'not defined' | 'period' | 'spotting'
+type CervicalMucusFeeling = 'nothing' | 'dry' | 'wet' | 'slippery'
+type CervicalMucusStructure = 'nothing' | 'creamy' | 'egg white'
+type CervixOpening = 'not defined' | 'closed' | 'medium' | 'raised'
+type CervixFirmness = 'not defined' | 'firm' | 'medium' | 'soft'
+type CervixPosition = 'not defined' | 'low' | 'medium' | 'high'
+type SexualActivityUsingContraceptive =
+  | 'none'
+  | 'condom'
+  | 'female condom'
+  | 'pill'
+  | 'ring'
+  | 'patch'
+  | 'hormonal IUD'
+  | 'copper IUD'
+  | 'chemical'
+  | 'diaphragm'
+  | 'implant'
+type SexualDesire = -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+type Pain =
+  | 'not defined'
+  | 'cramps'
+  | 'ovulation pain'
+  | 'headache'
+  | 'backache'
+  | 'nausea'
+  | 'tender breasts'
+  | 'migraine'
+  | 'other'
+type Mood =
+  | 'not defined'
+  | 'happy'
+  | 'confident'
+  | 'calm'
+  | 'energetic'
+  | 'excited'
+  | 'PMS'
+  | 'mood swings'
+  | 'irritable'
+  | 'anxious'
+  | 'stressed'
+  | 'tired'
+  | 'sensitive'
+  | 'numb'
+  | 'sad'
+  | 'angry'
 
 interface DayData {
   temperature?: number
   excludeReason: ExcludeReasonValue
   bleeding: BleedingValue
+  cervicalMucusFeeling: CervicalMucusFeeling
+  cervicalMucusStructure: CervicalMucusStructure
+  cervixOpening: CervixOpening
+  cervixFirmness: CervixFirmness
+  cervixPosition: CervixPosition
+  sexActivity: SexualActivityUsingContraceptive[]
+  sexDesire: SexualDesire
+  pain: Pain
+  mood: Mood
+  ovulationTest: boolean
 }
 
 const PropertyHeadline = ({ children }: { children: React.ReactNode }) => (
@@ -25,9 +80,19 @@ export default ({ onAdd }: { onAdd: (day: Day) => void }) => {
     temperature: undefined,
     excludeReason: 'not defined',
     bleeding: 'not defined',
+    cervicalMucusFeeling: 'nothing',
+    cervicalMucusStructure: 'nothing',
+    cervixOpening: 'not defined',
+    cervixFirmness: 'not defined',
+    cervixPosition: 'not defined',
+    sexActivity: new Array(),
+    sexDesire: -1,
+    pain: 'not defined',
+    mood: 'not defined',
+    ovulationTest: undefined,
   })
 
-  const { excludeReason, bleeding } = state
+  const { excludeReason, bleeding, cervicalMucusFeeling } = state
 
   const setTemperature = (newTemperature: number) => {
     setState({ ...state, temperature: newTemperature })
@@ -43,6 +108,14 @@ export default ({ onAdd }: { onAdd: (day: Day) => void }) => {
     setState({
       ...state,
       bleeding: newBleeding,
+    })
+
+  const setCervicalMucusFeeling = (
+    newCervicalMucusFeeling: CervicalMucusFeeling,
+  ) =>
+    setState({
+      ...state,
+      cervicalMucusFeeling: newCervicalMucusFeeling,
     })
 
   return (
@@ -98,6 +171,33 @@ export default ({ onAdd }: { onAdd: (day: Day) => void }) => {
           {
             title: 'Spotting',
             value: 'spotting',
+            icon: 'water',
+          },
+        ]}
+      />
+
+      <PropertyHeadline>CervicalMucus</PropertyHeadline>
+
+      <RadioButtonGroup
+        value={cervicalMucusFeeling}
+        onValueChange={(value) => {
+          setCervicalMucusFeeling(value as CervicalMucusFeeling)
+        }}
+        // TODO pick fitting icons
+        buttons={[
+          {
+            title: 'Dry',
+            value: 'dry',
+            icon: 'water',
+          },
+          {
+            title: 'Wet',
+            value: 'wet',
+            icon: 'water',
+          },
+          {
+            title: 'Slippery',
+            value: 'slippery',
             icon: 'water',
           },
         ]}
