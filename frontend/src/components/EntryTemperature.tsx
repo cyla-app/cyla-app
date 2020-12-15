d194c06bfc571575fc90a1fe6adaa8c52a37bbe0
Maximilian Ammann <max@maxammann.org>
2020-12-15 19:10:31 +0100

    Add missing entry

---
+++ frontend/src/components/EntryTemperature.tsx
@@ -0,0 +1,61 @@
+import { Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { useTheme } from 'react-native-paper'
+
+type PropsType = {
+  initialString: string
+  onTemperatureChanged: (temperature: number) => void
+}
+
+export default ({ initialString, onTemperatureChanged }: PropsType) => {
+  const [temperatureString, setTemperatureString] = useState<string>(
+    initialString,
+  )
+
+  const setTemperature = (
+    newTemperature: number,
+    newTemperatureString: string,
+  ) => {
+    onTemperatureChanged(newTemperature)
+    setTemperatureString(newTemperatureString)
+  }
+
+  const theme = useTheme()
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+      }}>
+      <View
+        style={{
+          flex: 0.25,
+        }}
+      />
+      <TextInput
+        style={{
+          width: 100,
+          fontWeight: 'bold',
+          fontFamily: 'monospace',
+          fontSize: 30,
+          alignSelf: 'center',
+        }}
+        selectionColor={theme.colors.primary}
+        value={temperatureString}
+        onChangeText={(text) => {
+          setTemperature(parseFloat(text), text)
+        }}
+        maxLength={5}
+        placeholder={'--.--'}
+        keyboardType={'numeric'}
+      />
+      <Text
+        style={{
+          flex: 0.25,
+          fontWeight: 'bold',
+          fontSize: 15,
+        }}>
+        ° C
+      </Text>
+    </View>
+  )
+}
