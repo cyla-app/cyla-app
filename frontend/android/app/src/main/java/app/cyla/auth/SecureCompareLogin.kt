a3cee1db230705f51d7c7bfafce3684c601cccbe
Maximilian Ammann <max@maxammann.org>
2021-02-16 14:48:14 +0100

    Fix login

--- frontend/android/app/src/main/java/app/cyla/auth/SecureCompareLogin.kt
+++ frontend/android/app/src/main/java/app/cyla/auth/SecureCompareLogin.kt
@@ -72,8 +72,8 @@ class SecureCompareLogin {
 
     suspend fun login(username: String, authKey: ByteArray, url: URL): SuccessAuthInfo {
         val host = url.host
-        val protocol = if (url.protocol === "https") "wss" else "ws"
-        val port = if (url.port == -1) ( if (url.protocol === "https") 443 else 80) else url.port
+        val protocol = if (url.protocol == "https") "wss" else "ws"
+        val port = if (url.port == -1) (if (url.protocol == "https") 443 else 80) else url.port
         
         return suspendCoroutine { cont: Continuation<SuccessAuthInfo> ->
             val wsListener = LoginWebSocketListener(authKey, cont)
