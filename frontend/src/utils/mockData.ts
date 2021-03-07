4e8f827ab6622ffba5e90d8f3cfe9f6125e417d0
Maximilian Ammann <max@maxammann.org>
2021-03-07 14:59:53 +0100

    Improve mock data generation

--- frontend/src/utils/mockData.ts
+++ frontend/src/utils/mockData.ts
@@ -8,7 +8,15 @@ import {
   MucusFeeling,
   MucusTexture,
   ExcludeReason,
+  CervixOpening,
+  CervixFirmness,
 } from '../types'
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 export const generateMockData = (dispatch: Dispatch) => {
   const randomDate = (start: Date, end: Date) =>
@@ -16,29 +24,67 @@ export const generateMockData = (dispatch: Dispatch) => {
       start.getTime() + Math.random() * (end.getTime() - start.getTime()),
     )
 
-  const random = randomDate(new Date(2020, 0, 1), new Date(2020, 2, 1))
+  const startDate = randomDate(new Date(2020, 0, 1), new Date(2020, 2, 1))
+  let day = 0
   const days: Day[] = []
-  for (let i = 0; i < 365; i++) {
-    const day = addDays(random, i)
-    days.push({
-      date: formatDay(day),
-      bleeding:
-        getDate(day) <= 10 && getDate(day) >= 7
-          ? {
-              strength: BleedingStrength.STRENGTH_STRONG,
-            }
-          : undefined,
-      temperature: {
-        value: 36.5 + 0.5 * Math.sin(Math.sin(0.1 * i) * i),
-        timestamp: day.toISOString(),
-        note: '',
-        excludeReason: ExcludeReason.EXCLUDE_REASON_NONE,
-      },
-      mucus: {
-        feeling: MucusFeeling.FEELING_DRY,
-        texture: MucusTexture.TEXTURE_EGG_WHITE,
-      },
-    })
+  for (let cycle = 0; cycle < 24; cycle++) {
+    const cycleLength = getRandomInt(21, 35)
+    const periodLength = getRandomInt(4, 6)
+    for (let cycleDay = 0; cycleDay < cycleLength; cycleDay++) {
+      const date = addDays(startDate, day)
+      day++
+      days.push({
+        date: formatDay(date),
+        bleeding:
+          cycleDay === 0
+            ? {
+                strength: BleedingStrength.STRENGTH_WEAK,
+              }
+            : cycleDay === periodLength - 2
+            ? {
+                strength: BleedingStrength.STRENGTH_MEDIUM,
+              }
+            : cycleDay === periodLength - 1
+            ? {
+                strength: BleedingStrength.STRENGTH_WEAK,
+              }
+            : cycleDay < periodLength
+            ? {
+                strength: BleedingStrength.STRENGTH_STRONG,
+              }
+            : undefined,
+        temperature: {
+          value: 37 + Math.sin(Math.sin(0.1 * cycle) * cycle),
+          timestamp: date.toISOString(),
+          note: '',
+          excludeReason: ExcludeReason.EXCLUDE_REASON_NONE,
+        },
+        mucus: {
+          feeling: getRandomInt(
+            MucusFeeling.FEELING_NONE,
+            MucusFeeling.FEELING_SLIPPERY,
+          ),
+          texture: getRandomInt(
+            MucusTexture.TEXTURE_NONE,
+            MucusTexture.TEXTURE_EGG_WHITE,
+          ),
+        },
+        cervix: {
+          position: getRandomInt(
+            CervixOpening.OPENING_NONE,
+            CervixOpening.OPENING_RAISED,
+          ),
+          firmness: getRandomInt(
+            CervixFirmness.FIRMNESS_NONE,
+            CervixFirmness.FIRMNESS_SOFT,
+          ),
+          opening: getRandomInt(
+            CervixOpening.OPENING_NONE,
+            CervixOpening.OPENING_RAISED,
+          ),
+        },
+      })
+    }
   }
   dispatch(saveMockDays(days))
 }
